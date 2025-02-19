f140deb5613045a100db5f6aaf413fe4d3ccf2e8
Kei <withwhom0215@gmail.com>
2025-02-19 17:55:33 +0100

    add license in the header

--- src/vampy/automatedPreprocessing/vmtk_pointselector.py
+++ src/vampy/automatedPreprocessing/vmtk_pointselector.py
@@ -130,7 +130,8 @@ class vmtkPickPointSeedSelector(vmtkSeedSelector):  # pragma: no cover
 
         self.vmtkRenderer.Renderer.AddActor(surfaceActor)
 
-        text = 'Please position the mouse and press space to add the top of the region of interest, \'u\' to undo\n'
+        text = 'Please position the mouse and press space to add the top of the region of interest, \'u\' to undo
+'
         guiText = VtkText(text)
         self.vmtkRenderer.Renderer.AddActor(guiText.text)
 
